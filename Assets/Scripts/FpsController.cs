e496723f4ee85fe73a81f9b913c980cac50a8f8e
kfederici <143634514+kfederici@users.noreply.github.com>
2024-02-01 08:29:02 -0500

    update

---
+++ Assets/Scripts/FpsController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class FpsController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 1.0f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    public bool canMove = true;
+    CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
