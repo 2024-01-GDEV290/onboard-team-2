74736f887e7c2870b9483cbd00be1d5a8495fd72
rodneyrocket69 <ahoo4983@stu.raritanval.edu>
2024-02-03 16:07:28 -0500

    trying to get the timing down

--- Assets/gloveCollision.cs
+++ Assets/gloveCollision.cs
@@ -8,19 +8,24 @@ public class BoxingGloveCollision : MonoBehaviour
     public GameObject phaseTwo;
     public AudioClip punch1;
     public AudioClip crumble1;
+    public AudioClip crumble2;
     private AudioSource audioSource;
-
+    
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the object we collided with is the box
+        //**PHASE 1**
+        
+        // Check if the object we collided with is the phase 1 head
         if (collision.gameObject.name == "headBase")
         {
-            // Apply force to the box
+            
+            // Apply force to the head
             Vector3 forceDirection = collision.contacts[0].point - transform.position;
             forceDirection = -forceDirection.normalized; // Apply force in the opposite direction of the collision
             collision.rigidbody.AddForce(forceDirection * 500); // Modify the force magnitude as needed
@@ -35,16 +40,39 @@ public class BoxingGloveCollision : MonoBehaviour
             Destroy(collision.gameObject);
 
             
-
+            //spawn the shards
             if (headShattered != null)
             {
                 headShattered.SetActive(true);
             }
-
+            //spawn the phase two head
             if (phaseTwo != null)
             {
                 phaseTwo.SetActive(true);
+              
             }
+
+
+        }
+
+        if (collision.gameObject.name == "easterIslandPhase2")
+        {
+            // Apply force to the head
+            Vector3 forceDirection = collision.contacts[0].point - transform.position;
+            forceDirection = -forceDirection.normalized; // Apply force in the opposite direction of the collision
+            collision.rigidbody.AddForce(forceDirection * 500); // Modify the force magnitude as needed
+            Debug.Log("collision");
+
+            //play sounds
+            if (punch1 != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(punch1);
+                audioSource.PlayOneShot(crumble2);
+            }
+
+            
+            Destroy(collision.gameObject);
+
         }
     }
 }
