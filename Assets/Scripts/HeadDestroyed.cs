4de9eee39426b38c616f09a92ff473f656108e33
kfederici <143634514+kfederici@users.noreply.github.com>
2024-02-04 20:53:55 -0500

    heads updated

--- Assets/Scripts/HeadDestroyed.cs
+++ Assets/Scripts/HeadDestroyed.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class HeadDestroyed : MonoBehaviour
 {
     public GameObject thirdHit;
 
-    private void OnTriggerEnter(Collider other)
+    private async void OnTriggerEnter(Collider other)
     {
+        await Task.Delay(700);
         Vector3 oldPos = transform.position;
         Instantiate(thirdHit, oldPos, Quaternion.identity);
         Destroy(gameObject);
--- Assets/Scripts/ReplaceHead.cs
+++ Assets/Scripts/ReplaceHead.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class ReplaceHead : MonoBehaviour
 {
     public GameObject head;
-    private void OnTriggerEnter(Collider other)
+    private async void OnTriggerEnter(Collider other)
     {
+        await Task.Delay(700);
         Vector3 oldPos = transform.position;
         Instantiate(head, oldPos, Quaternion.identity);
         Destroy(gameObject);
